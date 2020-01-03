ba82824cd2ce0e53316f68645074a89942d121f4
AsmaeAzz2018 <asmae.azz@gmail.com>
2020-01-03 14:45:04 +0100

    delegates and datasources methods

---
+++ ToDoL/TableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ToDoL
+//
+//  Created by mac on 1/3/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController{
+
+    let itemArray = ["Find Keys" , "Broke Heart" , "Destroy Head"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+       return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+
+
+}
+
