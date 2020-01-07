e1a900b6094efe0324527506e057efbc4e26cd95
AsmaeAzz2018 <asmae.azz@gmail.com>
2020-01-07 19:41:58 +0100

    persistence with encoding protocol and plist

---
+++ ToDoL/Controllers/TableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  ToDoL
+//
+//  Created by mac on 1/3/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController{
+
+    var itemArray = [Item]()
+   // let defaults = UserDefaults.standard
+     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        print(dataFilePath!)
+        loadItems()
+
+        
+//        if let items = defaults.array(forKey: "ToDoList")as? [String] {
+//            itemArray = items
+//        }
+       
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+       let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+       
+       return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print(itemArray[indexPath.row])
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        saveItems()
+    }
+
+  
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+           
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.saveItems()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new item ..."
+            textField = alertTextField
+           
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }catch {
+            print("Error encoding item array, \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self,from: data)
+            }catch {
+                print("Error decoding item array, \(error)")
+            }
+            
+        }
+    }
+    
+}
+
---
+++ ToDoL/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  CustomDataModel.swift
+//  ToDoL
+//
+//  Created by mac on 1/6/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title : String = ""
+    var done : Bool = false
+}
